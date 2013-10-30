ef91eb7ef32da7164713be8812398a6f21f43298
Bob Renwick <bob@editd.com>
2013-10-30 15:38:12 +0000

    adding first setup.py

---
+++ pyretry/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='pyretry',
+    version="0.1",
+    description='Separate your retry logic from your buxiness lgic',
+    author='Bob Renwick',
+    author_email='bob.renwick@gmail.com',
+    url='https://github.com/bobbyrenwick/pyretry',
+    packages=['pyretry'],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
